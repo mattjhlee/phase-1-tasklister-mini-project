8c1d1b7cb961dd475b04b52acc365447de740192
Matthew <mattjhlee25@gmail.com>
2023-05-03 15:31:28 -0400

    make able to add to list

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.getElementById("create-task-form");
+
+  form.addEventListener("submit", formSubmit, false);
+
+  function formSubmit(event) {
+    event.preventDefault();
+    //console.log(document.querySelector('#new-task-description').value);
+    const newTask = document.querySelector('#new-task-description').value;
+    const newLi = document.createElement("li");
+    newLi.textContent = newTask;
+    const workingList = document.querySelector("#tasks");
+    workingList.appendChild(newLi);
+  }
 });
